3d3dc144017f5421b221484b39f3cf887d9115a6
amirreza-torbat <44323335+amirreza-torbat@users.noreply.github.com>
2026-08-05 11:59:01 +0330

    Add files via upload

---
+++ GridConnectedPVData.m
@@ -0,0 +1,8 @@
+*** Power rating input from the user = 35.00 kW 
+*** Minimum number of panel required per string = 33 
+*** Maximum number of panel connected per string without reaching maximum system voltage = 41 
+*** Minimum power rating of the boost-less solar PV plant = 7.43 kW 
+*** Maximum power possible per string without reaching maximum DC voltage = 9.23 kW 
+*** Actual number of panel per string = 39 
+*** Number of strings connected in parallel = 4 
+*** Actual solar PV plant power = 35.12 kW
